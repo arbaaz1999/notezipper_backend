2b57dc4767136dceb5063c726b2cf72bed724dda
arbaaz1999 <arbaazmansuri09@gmail.com>
2022-11-24 22:48:33 +0530

    noteController

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -4,6 +4,7 @@ const asyncHandler = require('express-async-handler')
 
 const getNotes = asyncHandler(
     async (req, res) => {
+        
         const notes = await Note.find({})
         res.json(notes)
     }
