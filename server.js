b0fbdc2747f52440e04734991397219dd4af197e
arbaaz1999 <arbaazmansuri09@gmail.com>
2023-01-11 22:46:21 +0530

    updated

--- server.js
+++ server.js
@@ -1,37 +1,43 @@
-const fs = require('fs');
-const https = require('https');
-const express = require('express');
-const dotenv = require('dotenv')
-const cors = require('cors')
-const connectDB = require('./config/db')
-const userRoutes = require('./routes/userRoutes')
-const noteRoutes = require('./routes/noteRoutes')
+const fs = require("fs");
+const https = require("https");
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const noteRoutes = require("./routes/noteRoutes");
+const path = require("path");
 
-const app = express()
-app.use(cors())
-dotenv.config()
+const app = express();
+app.use(cors());
+dotenv.config();
 connectDB();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
+app.use("/api/users", userRoutes);
+app.use("/api/notes", noteRoutes);
 
-/* app.post('/api/notes', (req, res) => {
-    res.json(notes)
-}) */
+// =========================== delployment starts ===========================
 
-app.use('/api/users', userRoutes)
-app.use('/api/notes', noteRoutes)
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("notezipper_frontend/build"));
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
+// =========================== delployment ends ============================
 
-const server = https.createServer({
-    key: fs.readFileSync(`./localhost-key.pem`, 'utf8'),
-    cert: fs.readFileSync(`./localhost.pem`, 'utf8')
-}, app);
+const port = process.env.PORT || 443;
 
-server.listen(443);
+const server = https.createServer(
+  {
+    key: fs.readFileSync(`./localhost-key.pem`, "utf8"),
+    cert: fs.readFileSync(`./localhost.pem`, "utf8"),
+  },
+  app
+);
 
-/* const PORT = process.env.PORT || 5000
-
-app.listen(PORT, console.log(`port started on Port ${PORT}`)) */
+server.listen(port);
